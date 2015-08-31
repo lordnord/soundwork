29bef5cfeddcc226cd0ea93d8b5ebb94e922353c
Gulam Sheri <sherigulam@gmail.com>
2015-09-01 04:46:07 +0600

    Renamed process() to sine(), some minor changes

--- gen8mono.py
+++ gen8mono.py
@@ -1,46 +1,46 @@
 import wave
+import struct
 import random
 import math
 
 self = wave.open('new.wav', 'w')
 LEN = 44100
-PARAMS = (1, 2, 44100, LEN, 'NONE', 'not compressed')
+PARAMS = (1, 1, 44100, LEN, 'NONE', 'not compressed')
 self.setparams(PARAMS)
 
 qrate = self.getframerate() / 360.0
 
+def getwavelength(freq):
+    """
+    Returns wave length in samples for freq.
+    """
+    res = self.getframerate() / freq
+    return res
 
 def amplit(percent):
     full = 2 ** (8 ** self.getsampwidth()) / 2 - 1
     out = percent * full /100.0
     return int(out)
     
-def process(pos):
+def sine(pos, freq=50, amp=127):
     """
-    Do something with sample as an integers in range 0-255
+    Generates a sine wave with frequency = 50 and amplitude = 127.
     """
-    # if sample == 0:
-    #     res = 0
-    # else:
-        # res = sample * 2
     
-    # Generates a sine wave with frequency = 50 and amplitude = 127.
-    freq = 50
-    amp = 127
     res = math.sin(math.radians(pos * freq) / qrate) * amp
     
-        
     if res > 127:
         res = 127
     elif res < -128:
         res = -128
     return int(res)
+process = sine
 
 print 'End point is', LEN
 
 for i in range(LEN):
     # print '\r', i,
-    x = process(i) + 128
-    self.writeframes(chr(x))
+    x = sine(i) + 128
+    self.writeframes(struct.pack('B', x))
     
 self.close()
