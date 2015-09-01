7f00c221f44f0efbd7584bce17ba09281805d3ac
lordnord <dlx1989@gmail.com>
2015-09-01 03:01:19 +0300

    fix

--- new_gen8.py
+++ new_gen8.py
@@ -7,17 +7,12 @@ import wavefile
 def proc(file, pos):
     """Generates a sine wave with frequency = 50 and amplitude = 127"""
     freq = 50
-    amp = 127
     qrate = file.getframerate() / 360.0
-    res = math.sin(math.radians(pos * freq) / qrate) * amp
-            
-    if res > 127:
-        res = 127
-    elif res < -128:
-        res = -128
+    res = math.sin(math.radians(pos * freq) / qrate) * file.amplit(100)
+
     return int(res)
 
-new = wavefile.open('build1.wav', 'w')
+new = wavefile.open('build2.wav', 'w')
 new.set(8, 44100, 1000)
 new.gen(proc)
 del new
