df9ef54632e72f506103db86afa68af49e637c8e
Gulam Sheri <sherigulam@gmail.com>
2015-09-02 22:49:49 +0600

    First example of highlevel MIDI sequence

--- new_gen8.py
+++ new_gen8.py
@@ -1,18 +1,25 @@
 import random
 import math
-
 import wavefile
-
 
 def proc(file, pos):
     """Generates a sine wave with frequency = 50 and amplitude = 127"""
     freq = 6050
     qrate = file.getframerate() / 360.0
     res = math.sin(pos * freq) * file.amplit(100)
-
     return int(res)
 
+
+BPM = 70
+sequence = """
+1/8 E6 D#6 E6 D#6 E6 B5 D6 C6 3/8 A5 1/8 P 
+E5 A5 3/8 B5 1/8 E5 G#5 B5 3/8 C6
+"""
+
+
 new = wavefile.open('build2.wav', 'w')
-new.set(8, 44100, 1000)
-new.gen(proc)
+# lenght = new.len(sequence)
+lenght = 1000
+new.set(8, 44100, lenght)
+new.gen(sequence)
 del new
