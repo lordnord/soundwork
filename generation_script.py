36f4f2d00c73e0a676e7ab0db466f7d9af4f05c4
Gulam Sheri <sherigulam@gmail.com>
2015-09-03 16:45:29 +0600

    Using new function

--- generation_script.py
+++ generation_script.py
@@ -4,7 +4,7 @@ melody = """100.C#.4 100.P 100.C#.4 100.P 100.C.4
 200.P 100.B.3 100.P 100.E.4 100.P 100.A.3 100.P 200.C.4 100.P """
 
 new = wavefile.open('buildy.wav', 'w')
-lenght = 7000
+lenght = notes.seqencelenght(melody * 4)
 new.set(8, 44100, lenght)
 
 for freq, length in notes.parser(melody*4):
