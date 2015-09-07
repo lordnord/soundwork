cc3e563f459b3310d0ae7f1bb18e1d9a27621c61
Gulam Sheri <sherigulam@gmail.com>
2015-09-08 01:02:03 +0600

    Fixed fix of previous param + melody changes

--- merging_script.py
+++ merging_script.py
@@ -4,18 +4,19 @@ from soundwork.waveforms import sine, square, merge
 #melody = """100.C#.4 100.P 100.C#.4 100.P 100.C.4
 #200.P 100.B.3 100.P 100.E.4 100.P 100.A.3 100.P 200.C.4 100.P """
 
-melody = "1/8 E6 D#6 E6 D#6 E6 B5 D6 C6 3/8 A5 1/8 _ E5 A5 3/8 B5 1/8 E5 G#5 B5 3/8 C6"
+melody = """1/8 E6 D#6 E6 D#6 E6 B5 D6 C6 3/8 A5 1/8 _ E5 A5
+3/8 B5 1/8 E5 G#5 B5 3/8 C6 _"""
 
 new = wavefile.open('build-merg.wav', 'w')
-new.set(8, 44100)
+new.set(8, 44100, 0)
 
 def merging():
-    for freq, length in notes.parser(melody, bpm=70):
+    for freq, length in notes.parser(melody, bpm=50):
         merged = merge(sine(freq), square(freq*4))
         new.gen(merged, length)
     
 def simple():
-    for freq, length in notes.parser(melody, bpm=70):
+    for freq, length in notes.parser(melody, bpm=50):
         new.gen(sine(freq), length)
     
 merging()
