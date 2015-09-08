01b238e850ed36e67de59d99c3cd5fb7d33b5f53
lordnord <dlx1989@gmail.com>
2015-09-08 03:26:55 +0300

    rework notes. old notation dropped

--- soundwork/notes.py
+++ soundwork/notes.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from fractions import Fraction
 
+PAUSE = '_'
 # Note frequencies for 10th octave
 NOTES = {
 'C'  : 16744.036,
@@ -15,64 +16,54 @@ NOTES = {
 'A'  : 28160.000,
 'A#' : 29834.481,
 'B'  : 31608.531,
+PAUSE: 0,
 }
 
 class NotationError(Exception):
     pass
 
-def single(note):
-    'Generates frequency by note sign and octave'
-    if isinstance(note, str):
-        sign, octave = note.split('.')
-        octave = int(octave)
-    else:
-        sign, octave = note
-    return NOTES[sign] / (2 ** (10-octave))
-
-
-def muslength(le, bpm=None):
-    # Returns note length in ms for BPM
-    if bpm == None:
-        bpm = muslength.last
-    measure = 60000.0 / bpm
-    return measure * le
-
-muslength.last = 70
-
-def msec_parser(melody):
-    melody = melody.strip().replace('\n', ' ').split(' ')
-    for note in melody:
-        if 'P' in note:
-            len = note.split('.')[0]
-            freq = 0
+class Note:
+    def __init__(self, note):
+        if isinstance(note, str):
+            if note == PAUSE:
+                self.sign = PAUSE
+                self.octave = None
+            else:
+                self.sign, self.octave = note[:-1], int(note[-1])
         else:
-            len, sign, octave = note.split('.')
-            freq = NOTES[sign] / (2 ** (10-int(octave)))
-        yield (freq, int(len))
+            self.sign, self.octave = note
+        self._updatefreq()
+        
+    def _updatefreq(self):
+        if self.sign == PAUSE:
+            self.freq = 0
+        else:
+            self.freq = NOTES[self.sign] / (2 ** (10-self.octave))
+        
+    def __str__(self):
+        if self.sign == PAUSE:
+            return self.sign
+        else:
+            return self.sign + str(self.octave)
 
-def bpm_parser(melody, bpm):
+
+def muslength(fraction_length, bpm=None):
+    'Returns note length in ms for BPM'
+    return 60000.0 * fraction_lenght / bpm
+
+
+def parser(melody, bpm, default_len=1/4):
     melody = melody.strip().replace('\n', ' ').split(' ')
-    if '/' not in melody[0]:
-        raise NotationError("Notes length isn't specified.")
         
     for record in melody:
+        if record == '':
+            continue
         if '/' in record:
-            note_len = Fraction(record) * 60000 / bpm
+            default_len = Fraction(record) * 60000.0 / bpm
             continue
             
-        if record in ('_', 'P'):
-            freq = 0
-        else:
-            sign, octave = record[:-1], record[-1]
-            freq = NOTES[sign] / (2 ** (10-int(octave)))
-            
-        yield (freq, int(note_len))
-    
-def parser(melody, bpm=None):
-    if bpm:
-        return bpm_parser(melody, bpm)
-    else:
-        return msec_parser(melody)
+        yield Note(record).freq, int(default_len)
+  
 
 def sequencelength(seq, bpm=None):
     return sum(x[1] for x in parser(seq, bpm))
